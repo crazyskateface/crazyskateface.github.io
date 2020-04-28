fe2dba051e17f2fa7f42bb1c49280d339deb2522
Travis Boettjer <travis.boettjer@scripps.com>
2020-04-28 19:37:43 -0400

    Updates

---
+++ precache-manifest.d01a5331bd047682eeb3b0fc691daaf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72253dd8cea45e260fc662c483943721",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7509e89ae4fcf13773b6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e60f5ff6c52ebc50e85a",
+    "url": "/static/js/2.9e8a2d93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9e8a2d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7509e89ae4fcf13773b6",
+    "url": "/static/js/main.be458667.chunk.js"
+  },
+  {
+    "revision": "625feeca90a5081f0940",
+    "url": "/static/js/runtime-main.55a88fcd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
